da4995f836596541ac663f38ae4fec2e24064af1
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-04-25 10:16:25 -0400

    primer commit, estructura base

---
+++ src/test/java/cl/duoc/taskflow/TaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
